55e13ee120e9758c2f37bcf425775ed30b670f9a
Andrey Skityba <me@skityba.ru>
2023-04-11 14:31:27 +0300

    Initial commit

---
+++ src/main/java/com/example/jasypt_generator/JasyptGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.jasypt_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasyptGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JasyptGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jasypt_generator/JasyptGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jasypt_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasyptGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
